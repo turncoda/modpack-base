c0d0ca77fb456d01e035b02599d5ca7e688594cc
turncoda <144861042+turncoda@users.noreply.github.com>
2026-07-28 17:54:29 -0400

    Fork from map-starter and update for pseudochef

--- deploy.bat
+++ deploy.bat
@@ -1,13 +1,7 @@
 @echo off
 
-REM Output pak filename.
-set pak_filename=my_level
-
 REM Point this to your Unreal Engine installation. (You probably don't have to modify this.)
 set run_uat=C:\Program Files\Epic Games\UE_5.1\Engine\Build\BatchFiles\RunUAT.bat
-
-REM Point this to your Pseudoregalia installation. (You probably don't have to modify this.)
-set pseudoregalia_folder=C:\Program Files (x86)\Steam\steamapps\common\Pseudoregalia
 
 if not exist "%run_uat%" (
   echo Error: script variable 'run_uat' is set to "%run_uat%" which does not exist. Please install Unreal Engine 5.1 and ensure this script is using a valid path to RunUAT.bat.
@@ -15,19 +9,14 @@ if not exist "%run_uat%" (
   exit /b 1
 )
 
-if not exist "%pseudoregalia_folder%" (
-  echo Error: script variable 'pseudoregalia_folder' is set to "%pseudoregalia_folder%" which does not exist. Please install Pseudoregalia and ensure this script is using a valid path to its root folder.
-  pause
-  exit /b 1
-)
-
 REM Cook and pak.
-call "%run_uat%" BuildCookRun -project="%~dp0\pseudoregalia.uproject" -platform=Win64 -cook -stage -pak
+call "%run_uat%" BuildCookRun -project="%~dp0\pseudoregalia.uproject" -platform=Win64 -cook
 
-echo Copying pakchunk1-Windows.pak to Pseudoregalia folder, renamed as %pak_filename%_p.pak
-copy "%~dp0\Saved\StagedBuilds\Windows\pseudoregalia\Content\Paks\pakchunk1-Windows.pak" "%pseudoregalia_folder%\pseudoregalia\Content\Paks\%pak_filename%_p.pak"
+REM Set this to where the cooked map should go.
+set dst=%~dp0
 
-echo Done!
+copy "%~dp0\Saved\Cooked\Windows\pseudoregalia\Content\Mods\Maps\mise.uexp" "%dst%\mise.uexp"
+copy "%~dp0\Saved\Cooked\Windows\pseudoregalia\Content\Mods\Maps\mise.umap" "%dst%\mise.umap"
 
-echo Running the game...
-"%pseudoregalia_folder%\pseudoregalia\Binaries\Win64\pseudoregalia-Win64-Shipping.exe"
+echo Cooked!
+pause
